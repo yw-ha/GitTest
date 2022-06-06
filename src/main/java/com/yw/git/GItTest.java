d1bbb0f8547b44f30260745634450845a8e2598d
yw <yw@gmail.com>
2022-06-06 18:05:36 +0800

    Third commit

--- src/main/java/com/yw/git/GItTest.java
+++ src/main/java/com/yw/git/GItTest.java
@@ -8,5 +8,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello github!");
+        System.out.println("hello git3!");
     }
 }
