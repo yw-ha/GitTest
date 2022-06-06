66aba9450d1a5423c65ca34e3ce6218fbcef6a6e
yw <yw@gmail.com>
2022-06-06 18:04:44 +0800

    Second commit

--- src/main/java/com/yw/git/GItTest.java
+++ src/main/java/com/yw/git/GItTest.java
@@ -7,5 +7,6 @@ package com.yw.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello github!");
     }
 }
