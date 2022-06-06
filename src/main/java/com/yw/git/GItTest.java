f905a7e56aaa9bba22f194f09ee8c66499ba84db
yw <yw@gmail.com>
2022-06-06 18:56:17 +0800

    push commit

--- src/main/java/com/yw/git/GItTest.java
+++ src/main/java/com/yw/git/GItTest.java
@@ -4,6 +4,7 @@ package com.yw.git;
  * @author yw
  * @create 2022-06-06 17:44
  */
+//token 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
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
@@ -12,5 +13,6 @@ public class GItTest {
         System.out.println("hello git4!");
         System.out.println("hello master!");
         System.out.println("hello hot-fix!");
+        System.out.println("push test");
     }
 }
