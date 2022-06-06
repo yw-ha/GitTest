1c79fdf27d08286c5668a2b8b3081a94b9c7c4a9
yw <yw@gmail.com>
2022-06-06 18:01:34 +0800

    first commit

---
+++ src/main/java/com/yw/git/GItTest.java
@@ -0,0 +1,11 @@
+package com.yw.git;
+
+/**
+ * @author yw
+ * @create 2022-06-06 17:44
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
